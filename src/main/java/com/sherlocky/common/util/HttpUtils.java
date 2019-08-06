762fd6dacc867bd44ca0f0abae1f2e95d22cfdf2
Sherlocky <y836097668@126.com>
2019-08-06 10:46:38 +0800

    重构部分代码

--- src/main/java/com/sherlocky/common/util/HttpUtils.java
+++ src/main/java/com/sherlocky/common/util/HttpUtils.java
@@ -1,41 +1,53 @@
 package com.sherlocky.common.util;
 
-import org.apache.commons.codec.binary.Base64;
+import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.StringUtils;
 
 import javax.servlet.http.HttpServletRequest;
 import java.io.UnsupportedEncodingException;
 import java.net.URLEncoder;
+import java.util.Base64;
+
+import static com.sherlocky.common.constant.CommonConstants.ENCODING;
 
 /**
  * http相关简单工具集
+ *
  * @author: zhangcx
  * @date: 2019/8/1 15:03
  */
+@Slf4j
 public class HttpUtils {
-
     /**
-     * 转换中文文件名编码（下载名称包含中文的文件时, 部分浏览器会显示乱码）
+     * 中文文件名编码转换（例如：下载文件文件名包含中文时, 部分浏览器会乱码）
+     *
      * @param request
      * @param fileName
      * @return
      */
-    public static String convert(HttpServletRequest request, String fileName) {
+    public static String convertFileName(HttpServletRequest request, String fileName) {
         if (fileName == null) {
-            throw new IllegalArgumentException("输入参数是null");
+            throw new IllegalArgumentException("文件名称参数为 null");
+        }
+        String agent = request.getHeader("USER-AGENT");
+        if (StringUtils.isBlank(agent)) {
+            return fileName;
         }
         try {
-            String agent = request.getHeader("USER-AGENT");
-            if (null != agent && (-1 != agent.indexOf("MSIE") || -1 != agent.indexOf("Trident") || -1 != agent.indexOf("Edge"))) {
-                return URLEncoder.encode(fileName, "UTF8").replace("+", "%20");
-            } else if (null != agent && -1 != agent.indexOf("Safari")) {
-                return new String(fileName.getBytes("utf-8"), "ISO8859-1");
-            } else if (null != agent && -1 != agent.indexOf("Mozilla")) {
-                return "=?UTF-8?B?" + (new String(Base64.encodeBase64(fileName.getBytes("UTF-8")))) + "?=";
-            } else {
-                return fileName;
+            // 微软系浏览器
+            if (StringUtils.containsAny(fileName, "MSIE", "Trident", "Edge")) {
+                return URLEncoder.encode(fileName, ENCODING).replace("+", "%20");
+            }
+            if (agent.contains("Safari")) {
+                return new String(fileName.getBytes(ENCODING), "ISO8859-1");
+            }
+            if (agent.contains("Mozilla")) {
+                // 此处使用Java8 原生的 Base64 类
+                return "=?UTF-8?B?" + (new String(Base64.getEncoder().encodeToString(fileName.getBytes(ENCODING)))) + "?=";
             }
         } catch (UnsupportedEncodingException ex) {
+            log.error("$$$$$$ 转换文件名编码失败！", ex);
+        } finally {
             return fileName;
         }
     }
@@ -60,21 +72,25 @@ public class HttpUtils {
                 }
             }
         }
-        if (StringUtils.isBlank(ip) || "unknown".equalsIgnoreCase(ip)) {
+        if (unknown(ip)) {
             ip = request.getHeader("Proxy-Client-IP");
         }
-        if (StringUtils.isBlank(ip) || "unknown".equalsIgnoreCase(ip)) {
+        if (unknown(ip)) {
             ip = request.getHeader("WL-Proxy-Client-IP");
         }
-        if (StringUtils.isBlank(ip) || "unknown".equalsIgnoreCase(ip)) {
+        if (unknown(ip)) {
             ip = request.getHeader("HTTP_CLIENT_IP");
         }
-        if (StringUtils.isBlank(ip) || "unknown".equalsIgnoreCase(ip)) {
+        if (unknown(ip)) {
             ip = request.getHeader("HTTP_X_FORWARDED_FOR");
         }
-        if (StringUtils.isBlank(ip) || "unknown".equalsIgnoreCase(ip)) {
+        if (unknown(ip)) {
             ip = request.getRemoteAddr();
         }
         return ip;
+    }
+
+    private static boolean unknown(String ip) {
+        return StringUtils.isBlank(ip) || "unknown".equalsIgnoreCase(ip);
     }
 }
