055044faa5ba5e7c9eb8021740c76589bc5f6d59
Sherlocky <y836097668@126.com>
2019-08-06 09:24:02 +0800

    http相关简单工具集更新

--- src/main/java/com/sherlocky/common/util/HttpUtils.java
+++ src/main/java/com/sherlocky/common/util/HttpUtils.java
@@ -1,19 +1,24 @@
 package com.sherlocky.common.util;
 
 import org.apache.commons.codec.binary.Base64;
+import org.apache.commons.lang3.StringUtils;
 
 import javax.servlet.http.HttpServletRequest;
 import java.io.UnsupportedEncodingException;
 import java.net.URLEncoder;
 
 /**
+ * http相关简单工具集
  * @author: zhangcx
  * @date: 2019/8/1 15:03
  */
 public class HttpUtils {
 
     /**
-     * 中文编码转换, 典型的情况是下载中文名的文件时, 浏览器不能正确地显示汉字
+     * 转换中文文件名编码（下载名称包含中文的文件时, 部分浏览器会显示乱码）
+     * @param request
+     * @param fileName
+     * @return
      */
     public static String convert(HttpServletRequest request, String fileName) {
         if (fileName == null) {
@@ -35,15 +40,39 @@ public class HttpUtils {
         }
     }
 
+    /**
+     * 获取客户端ip
+     *
+     * @param request
+     * @return
+     */
     public static String getClientIp(HttpServletRequest request) {
-        String ip = request.getHeader("x-forwarded-for");
-        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+        String ip = request.getHeader("X-Forwarded-For");
+        if (StringUtils.isNotBlank(ip) && !"unknown".equalsIgnoreCase(ip)) {
+            // 多次反向代理后会有多个IP值，第一个不为 unknow 的才为真实IP
+            String[] ips = StringUtils.split(ip, ",");
+            for (String i : ips) {
+                // 多个ip之间以「英文逗号 + 空格」隔开
+                i = StringUtils.trim(i);
+                if (StringUtils.isNotBlank(i) && !"unknown".equalsIgnoreCase(i)) {
+                    ip = i;
+                    break;
+                }
+            }
+        }
+        if (StringUtils.isBlank(ip) || "unknown".equalsIgnoreCase(ip)) {
             ip = request.getHeader("Proxy-Client-IP");
         }
-        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+        if (StringUtils.isBlank(ip) || "unknown".equalsIgnoreCase(ip)) {
             ip = request.getHeader("WL-Proxy-Client-IP");
         }
-        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+        if (StringUtils.isBlank(ip) || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getHeader("HTTP_CLIENT_IP");
+        }
+        if (StringUtils.isBlank(ip) || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getHeader("HTTP_X_FORWARDED_FOR");
+        }
+        if (StringUtils.isBlank(ip) || "unknown".equalsIgnoreCase(ip)) {
             ip = request.getRemoteAddr();
         }
         return ip;
