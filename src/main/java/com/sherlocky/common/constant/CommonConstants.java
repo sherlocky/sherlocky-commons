e0aebd7e6e050d4a1ca3a5dfb1a8d9528420706f
Sherlocky <y836097668@126.com>
2019-08-06 10:47:03 +0800

    常用常量

---
+++ src/main/java/com/sherlocky/common/constant/CommonConstants.java
@@ -0,0 +1,11 @@
+package com.sherlocky.common.constant;
+
+/**
+ * 常用的一些常量
+ * @author: zhangcx
+ * @date: 2019/8/6 10:33
+ */
+public class CommonConstants {
+    /** 常用编码，utf-8 */
+    public static final String ENCODING = "UTF-8";
+}
