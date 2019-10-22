253f7572e412789ce412d6efc0af7c6c920bc38e
Sherlock <y836097668@126.com>
2019-10-22 19:34:13 +0800

    v0.0.7 UUID工具类

---
+++ src/main/java/com/sherlocky/common/util/UUIDUtils.java
@@ -0,0 +1,23 @@
+package com.sherlocky.common.util;
+
+import org.apache.commons.lang3.StringUtils;
+
+import java.util.UUID;
+
+/**
+ * UUID 工具类
+ */
+public class UUIDUtils {
+
+    private UUIDUtils() {
+    }
+
+    public static String get() {
+        return StringUtils.replace(UUID.randomUUID().toString(), "-", "");
+    }
+
+    public static void main(String[] args) {
+        System.out.println(UUIDUtils.get());
+        System.out.println(UUIDUtils.get());
+    }
+}
