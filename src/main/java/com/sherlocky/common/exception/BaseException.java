1ba020419d960564a7a5fc57eeaf53176a52b7b6
Sherlock <y836097668@126.com>
2019-10-23 09:18:49 +0800

    v0.0.7 公用的Exception

---
+++ src/main/java/com/sherlocky/common/exception/BaseException.java
@@ -0,0 +1,25 @@
+package com.sherlocky.common.exception;
+
+/**
+ * 公用的Exception
+ * <p>
+ * 继承自RuntimeException
+ * </p>
+ */
+public class BaseException extends RuntimeException {
+    public BaseException() {
+        super();
+    }
+
+    public BaseException(String message) {
+        super(message);
+    }
+
+    public BaseException(Throwable cause) {
+        super(cause);
+    }
+
+    public BaseException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
