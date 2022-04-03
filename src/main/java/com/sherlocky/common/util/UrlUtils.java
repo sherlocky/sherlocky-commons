41d12beaf5ed639b391b9031fbb852e707257737
Sherlocky <y836097668@126.com>
2022-04-03 10:37:26 +0800

    添加url工具类

--- src/main/java/com/sherlocky/common/util/UrlUtils.java
+++ src/main/java/com/sherlocky/common/util/UrlUtils.java
@@ -1,37 +1,42 @@
 package com.sherlocky.common.util;
 
+import org.apache.commons.lang3.StringUtils;
+
+import static com.sherlocky.common.constant.CommonConstants.HTTPS_SCHEMA_PREFIX;
+import static com.sherlocky.common.constant.CommonConstants.HTTP_SCHEMA_PREFIX;
+
 /**
  * @author zhangxu
  * @date 2022/3/20
  */
-public class UrlUtils {
+public final class UrlUtils {
+
+    private UrlUtils() {}
+
+    public static void main(String[] args) {
+        System.out.println(UrlUtils.getRelativePathFromUrl("https://sherlocky.github.com/2022/03/30/index.html"));
+    }
 
     /**
      * <p>获取URL中相对于域名的绝对地址</p>
      * <p>
      * 例如：
-     * https://uniedudev.oss-cn-north-2.unicloudsrv.com/2020/09/10/e12d889dfb65501daa9b66f50a474fbe.ts
+     * https://sherlocky.github.com/2022/03/30/index.html
      * -->
-     * /2020/09/10/e12d889dfb65501daa9b66f50a474fbe.ts
-     * </p>
-     * <p>
-     * 例如：
-     * https://ydxx.sz.edu.cn/szyxai/2020/09/10/e12d889dfb65501daa9b66f50a474fbe.ts
-     * -->
-     * /szyxai/2020/09/10/e12d889dfb65501daa9b66f50a474fbe.ts
+     * /2022/03/30/index.html
      * </p>
      * @param url
      * @return
      */
-    private String getRelativePathFromUrl(String url) {
-        if (StrUtil.isBlank(url) || !url.startsWith("http")) {
+    public static String getRelativePathFromUrl(String url) {
+        if (StringUtils.isBlank(url) || !url.startsWith("http")) {
             return url;
         }
         int pathSeparatorIndex = url.startsWith(HTTPS_SCHEMA_PREFIX) ?
-                StrUtil.indexOf(url, "/", HTTPS_SCHEMA_PREFIX.length(), false)
+                StringUtils.indexOf(url, "/", HTTPS_SCHEMA_PREFIX.length())
                 :
-                StrUtil.indexOf(url, "/", HTTP_SCHEMA_PREFIX.length(), false)
+                StringUtils.indexOf(url, "/", HTTP_SCHEMA_PREFIX.length())
                 ;
-        return StrUtil.subSuf(url, pathSeparatorIndex);
+        return StringUtils.substring(url, pathSeparatorIndex);
     }
 }
