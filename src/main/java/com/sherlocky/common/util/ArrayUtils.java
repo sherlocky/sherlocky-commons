259b92318808fc01b64b91c10d98fd63e6f3fa42
Sherlocky <y836097668@126.com>
2022-07-10 20:41:11 +0800

    feat: 添加数组转String方法

--- src/main/java/com/sherlocky/common/util/ArrayUtils.java
+++ src/main/java/com/sherlocky/common/util/ArrayUtils.java
@@ -8,6 +8,7 @@ import java.util.stream.Collectors;
 /**
  * 数组相关工具类
  * @date 2022/5/21
+ * @since 0.8.0
  */
 public final class ArrayUtils {
 
@@ -35,6 +36,58 @@ public final class ArrayUtils {
             return Collections.EMPTY_LIST;
         }
         return Arrays.stream(array).collect(Collectors.toList());
+    }
+
+    /**
+     * 对象是否为数组对象
+     *
+     * @param obj 对象
+     * @return 是否为数组对象，如果为{@code null} 返回false
+     */
+    public static boolean isArray(Object obj) {
+        if (null == obj) {
+            return false;
+        }
+        return obj.getClass().isArray();
+    }
+
+    /**
+     * 数组转String
+     *
+     * @param obj 集合或数组对象
+     * @return 数组字符串，与集合转字符串格式相同
+     */
+    public static String toString(Object obj) {
+        if (null == obj) {
+            return null;
+        }
+
+        if(obj instanceof long[]){
+            return Arrays.toString((long[]) obj);
+        } else if(obj instanceof int[]){
+            return Arrays.toString((int[]) obj);
+        } else if(obj instanceof short[]){
+            return Arrays.toString((short[]) obj);
+        } else if(obj instanceof char[]){
+            return Arrays.toString((char[]) obj);
+        } else if(obj instanceof byte[]){
+            return Arrays.toString((byte[]) obj);
+        } else if(obj instanceof boolean[]){
+            return Arrays.toString((boolean[]) obj);
+        } else if(obj instanceof float[]){
+            return Arrays.toString((float[]) obj);
+        } else if(obj instanceof double[]){
+            return Arrays.toString((double[]) obj);
+        } else if (ArrayUtils.isArray(obj)) {
+            // 对象数组
+            try {
+                return Arrays.deepToString((Object[]) obj);
+            } catch (Exception ignore) {
+                //ignore
+            }
+        }
+
+        return obj.toString();
     }
 
 }
