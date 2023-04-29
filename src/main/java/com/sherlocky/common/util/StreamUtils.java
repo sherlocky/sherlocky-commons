fd3b780f81f3cc255b42bb350b20d77eec5ae0e7
Sherlocky <y836097668@126.com>
2023-04-29 15:25:54 +0800

    feat: Stream工具类新增合并List方法

--- src/main/java/com/sherlocky/common/util/StreamUtils.java
+++ src/main/java/com/sherlocky/common/util/StreamUtils.java
@@ -1,9 +1,25 @@
 package com.sherlocky.common.util;
 
+import java.util.Collection;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 /**
  * 新增Java Stream工具类
  * @author zhangxu
  * @date 2023/9/27
  */
 public class StreamUtils {
+
+    /**
+     * 合并List
+     * @param list
+     * @return
+     * @param <E>
+     */
+    public <E> List<E> concatList(List<E>... list) {
+        return Stream.of(list).flatMap(Collection::stream).collect(Collectors.toList());
+    }
+
 }
