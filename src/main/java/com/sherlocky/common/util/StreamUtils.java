8a8afa93e2163cdd3a66f44174443c811ddda65d
Sherlocky <y836097668@126.com>
2023-05-06 14:06:05 +0800

    feat: Stream工具类新增list分组方法

--- src/main/java/com/sherlocky/common/util/StreamUtils.java
+++ src/main/java/com/sherlocky/common/util/StreamUtils.java
@@ -1,7 +1,11 @@
 package com.sherlocky.common.util;
 
+import org.checkerframework.checker.units.qual.K;
+
 import java.util.Collection;
 import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -20,6 +24,18 @@ public class StreamUtils {
      */
     public <E> List<E> concatList(List<E>... list) {
         return Stream.of(list).flatMap(Collection::stream).collect(Collectors.toList());
+    }
+
+    /**
+     * list分组
+     * @param list
+     * @param keyFunction
+     * @return
+     * @param <K>
+     * @param <E>
+     */
+    public <K, E> Map<K, List<E>> toListMap(List<E> list, Function<E, K> keyFunction) {
+        return list.stream().collect(Collectors.groupingBy(keyFunction));
     }
 
 }
