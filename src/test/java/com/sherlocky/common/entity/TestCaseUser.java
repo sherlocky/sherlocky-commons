a513c837fe4a8b67702cfc7c05a5273e2304e019
Sherlocky <y836097668@126.com>
2023-09-09 18:42:37 +0800

    test: 拆分一个TestCaseUser类方便单元测试

---
+++ src/test/java/com/sherlocky/common/entity/TestCaseUser.java
@@ -0,0 +1,46 @@
+package com.sherlocky.common.entity;
+
+public class TestCaseUser {
+    private Long id;
+    private String name;
+
+    private String email;
+
+    public TestCaseUser() {
+    }
+
+    public TestCaseUser(Long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public TestCaseUser(Long id, String name, String email) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
--- src/test/java/com/sherlocky/common/util/SortUtilsTest.java
+++ src/test/java/com/sherlocky/common/util/SortUtilsTest.java
@@ -1,6 +1,7 @@
 package com.sherlocky.common.util;
 
 import com.google.common.collect.Lists;
+import com.sherlocky.common.entity.TestCaseUser;
 import org.apache.commons.lang3.RandomStringUtils;
 import org.junit.Test;
 
@@ -16,52 +17,25 @@ public class SortUtilsTest {
     public void test() {
         List<Long> userIds = Lists.newArrayList(102L, 103L, 101L);
 
-        List<User> users = new ArrayList<>();
+        List<TestCaseUser> users = new ArrayList<>();
         for (int i = 0; i < userIds.size(); i++) {
-            User user = new User(userIds.get((i + 2) % 3), RandomStringUtils.randomAlphanumeric(10));
+            TestCaseUser user = new TestCaseUser(userIds.get((i + 2) % 3), RandomStringUtils.randomAlphanumeric(10));
             users.add(user);
         }
-        System.out.println("原User List排序：");
+        System.out.println("原TestCaseUser List排序：");
         System.out.println(JsonUtils.toJSONString(users));
 
         System.out.println("userIds排序：");
         System.out.println(JsonUtils.toJSONString(userIds));
 
-        List<User> sortedUsers = SortUtils.sortByProvideFieldOrder(users, userIds, User::getId);
-        System.out.println("User List排序后：");
-        System.out.println(JsonUtils.toJSONString(sortedUsers));
+        List<TestCaseUser> sortedTestCaseUsers = SortUtils.sortByProvideFieldOrder(users, userIds, TestCaseUser::getId);
+        System.out.println("TestCaseUser List排序后：");
+        System.out.println(JsonUtils.toJSONString(sortedTestCaseUsers));
 
         users.remove(0);
-        List<User> sortedUsers2 = SortUtils.sortByProvideFieldOrder(users, userIds, User::getId, false);
-        System.out.println("User List 删除一个元素，排序后：");
-        System.out.println(JsonUtils.toJSONString(sortedUsers2));
+        List<TestCaseUser> sortedTestCaseUsers2 = SortUtils.sortByProvideFieldOrder(users, userIds, TestCaseUser::getId, false);
+        System.out.println("TestCaseUser List 删除一个元素，排序后：");
+        System.out.println(JsonUtils.toJSONString(sortedTestCaseUsers2));
     }
-
-    public class User {
-        private Long id;
-        private String name;
-
-        public User(Long id, String name) {
-            this.id = id;
-            this.name = name;
-        }
-
-        public Long getId() {
-            return id;
-        }
-
-        public void setId(Long id) {
-            this.id = id;
-        }
-
-        public String getName() {
-            return name;
-        }
-
-        public void setName(String name) {
-            this.name = name;
-        }
-    }
-
 
 }
