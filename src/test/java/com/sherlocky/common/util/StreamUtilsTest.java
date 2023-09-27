eb78a09a46e3df66ffd238c3ef0ab25b90807e97
Sherlocky <y836097668@126.com>
2023-09-27 15:05:56 +0800

    test: StreamUtils单元测试方法

--- src/test/java/com/sherlocky/common/util/StreamUtilsTest.java
+++ src/test/java/com/sherlocky/common/util/StreamUtilsTest.java
@@ -1,12 +1,32 @@
 package com.sherlocky.common.util;
 
+import com.google.common.collect.Lists;
+import com.sherlocky.common.entity.TestCaseUser;
+import org.apache.commons.lang3.RandomStringUtils;
 import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class StreamUtilsTest {
 
     @Test
     public void test() {
+        List<Long> userIds = Lists.newArrayList(101L, 102L, 103L);
+        List<TestCaseUser> users = new ArrayList<>();
+        for (int i = 0; i < userIds.size(); i++) {
+            TestCaseUser user = new TestCaseUser(userIds.get(i), RandomStringUtils.randomAlphanumeric(10));
+            users.add(user);
+        }
+        System.out.println("原list：" + JsonUtils.toJSONString(users));
 
+        System.out.println("concatList：" + JsonUtils.toJSONString(StreamUtils.concatList(users, users)));
+
+        System.out.println("按ID分组：" + JsonUtils.toJSONString(StreamUtils.groupByKey(users, TestCaseUser::getId)));
+        System.out.println("按name分组：" + JsonUtils.toJSONString(StreamUtils.groupByKey(users, TestCaseUser::getName)));
+
+        System.out.println("toIdentityMap：" + JsonUtils.toJSONString(StreamUtils.toIdentityMap(users, TestCaseUser::getId)));
+        System.out.println("toMap：" + JsonUtils.toJSONString(StreamUtils.toMap(users, TestCaseUser::getId, TestCaseUser::getName)));
     }
 
 }
