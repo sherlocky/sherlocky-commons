e7f9de3fd24a3d2cad4091521b32848ba9d0abcb
Sherlocky <y836097668@126.com>
2023-10-07 21:38:26 +0800

    test: 完善单元测试

--- src/test/java/com/sherlocky/common/util/StreamUtilsTest.java
+++ src/test/java/com/sherlocky/common/util/StreamUtilsTest.java
@@ -6,7 +6,9 @@ import org.apache.commons.lang3.RandomStringUtils;
 import org.junit.Test;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class StreamUtilsTest {
 
@@ -27,6 +29,23 @@ public class StreamUtilsTest {
 
         System.out.println("toIdentityMap：" + JsonUtils.toJSONString(StreamUtils.toIdentityMap(users, TestCaseUser::getId)));
         System.out.println("toMap：" + JsonUtils.toJSONString(StreamUtils.toMap(users, TestCaseUser::getId, TestCaseUser::getName)));
+    }
+
+    @Test
+    public void testMapMerge() {
+        List<Long> userIds = Lists.newArrayList(101L, 102L, 103L);
+        List<TestCaseUser> users = new ArrayList<>();
+        for (int i = 0; i < userIds.size(); i++) {
+            TestCaseUser user = new TestCaseUser(userIds.get(i), RandomStringUtils.randomAlphanumeric(10));
+            users.add(user);
+        }
+        Map<String, Long> userMap = new HashMap<>();
+        users.forEach(u -> userMap.merge(
+                u.getName(),
+                u.getId(),
+                Long::sum));
+        System.out.println(JsonUtils.toJSONString(userMap));
+
     }
 
 }
