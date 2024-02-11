6d7b2f4c2decc76a4fd45978872529f25a5d5bca
Sherlocky <y836097668@126.com>
2024-02-11 10:34:15 +0800

    feat: SSLPoke增加输出支持的协议

--- src/test/java/com/sherlocky/common/util/SSLPoke.java
+++ src/test/java/com/sherlocky/common/util/SSLPoke.java
@@ -34,6 +34,16 @@ public class SSLPoke {
 			}
 			System.out.println("Successfully connected");
 
+			String[] protocols = sslsocket.getSupportedProtocols();
+			System.out.println("Supported Protocols: " + protocols.length);
+			for (int i = 0; i < protocols.length; i++) {
+				System.out.println(" " + protocols[i]);
+			}
+			protocols = sslsocket.getEnabledProtocols();
+			System.out.println("Enabled Protocols: " + protocols.length);
+			for (int i = 0; i < protocols.length; i++) {
+				System.out.println(" " + protocols[i]);
+			}
 		} catch (Exception exception) {
 			exception.printStackTrace();
 		}
